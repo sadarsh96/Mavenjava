c9bf76570596bfc9eb2a8d5c75ff4430b9ae9447
Adarsh <spunit96@gmail.com>
2022-02-28 15:38:17 +0530

    first commit

---
+++ src/test/java/QAClickacademy/Mavenjava/APITest.java
@@ -0,0 +1,18 @@
+package QAClickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+
+
+public class APITest {
+
+	@Test
+	public void apiAutomatiFirstMethod() {
+		System.out.println("apiAutomatiFirstMethod");
+	}
+	@Test
+	public void apiAutomationSecondMethod() {
+		System.out.println("apiAutomationSecondMethod");
+	}
+	
+}
---
+++ src/test/java/QAClickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package QAClickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+
+
+
+public class AppiumTest {
+
+	@Test
+	public void appiumAutomationFirstMethod() {
+		System.out.println("appiumAutomationFirstMethod");
+	}
+	@Test
+	public void appiumAutomationSecondMethod() {
+		System.out.println("appiumAutomationSecondMethod");
+	}
+}
